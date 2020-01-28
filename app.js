51a49c9ce49741a08ef69ee36996ccda99fa4339
Ophir Yael <ofir.yael@gmail.com>
2020-01-28 22:34:12 +0200

    Use object type and inference

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
-function add(n1, n2, showResult, phrase) {
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return result;
-    }
-}
-var number1;
-number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = 'Result is: ';
-add(number1, number2, printResult, resultPhrase);
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+var person = {
+    name: 'Ophir',
+    age: 30
+};
+console.log(person.name);
