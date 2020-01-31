0758042794166065888a2e543befa308c73bf5be
Ophir Yael <ofir.yael@gmail.com>
2020-01-31 21:52:54 +0200

    Using the this keyword in methods

--- dist/app.js
+++ dist/app.js
@@ -3,8 +3,13 @@ var Department = /** @class */ (function () {
     function Department(n) {
         this.name = n;
     }
+    Department.prototype.describe = function () {
+        console.log('Department: ' + this.name);
+    };
     return Department;
 }());
 var accounting = new Department('Accounting');
-console.log(accounting);
+accounting.describe();
+var accountingCopy = { name: 'DUMMEY', describe: accounting.describe };
+accountingCopy.describe();
 //# sourceMappingURL=app.js.map
