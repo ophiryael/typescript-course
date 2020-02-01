35dfc8feba49f82077082ec606197f6749e20e6a
Ophir Yael <ofir.yael@gmail.com>
2020-02-01 22:33:07 +0200

    Use private and public access modifiers

--- dist/app.js
+++ dist/app.js
@@ -1,15 +1,28 @@
 "use strict";
 var Department = /** @class */ (function () {
     function Department(n) {
+        this.employees = [];
         this.name = n;
     }
     Department.prototype.describe = function () {
         console.log('Department: ' + this.name);
     };
+    Department.prototype.addEmployee = function (employee) {
+        this.employees.push(employee);
+    };
+    Department.prototype.printEmployeeInformation = function () {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    };
     return Department;
 }());
 var accounting = new Department('Accounting');
+accounting.addEmployee('Dumbledore');
+accounting.addEmployee('Gandalf');
+// accounting.employees[2] = 'Anna';
 accounting.describe();
-var accountingCopy = { name: 'DUMMEY', describe: accounting.describe };
-accountingCopy.describe();
+accounting.name = 'NEW NAME';
+accounting.printEmployeeInformation();
+// const accountingCopy = { name: 'DUMMEY', describe: accounting.describe };
+// accountingCopy.describe();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 class Department {
-  name: string;
+  public name: string;
+  private employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
@@ -8,10 +9,24 @@ class Department {
   describe(this: Department) {
     console.log('Department: ' + this.name);
   }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 const accounting = new Department('Accounting');
+accounting.addEmployee('Dumbledore');
+accounting.addEmployee('Gandalf');
+// accounting.employees[2] = 'Anna';
 accounting.describe();
+accounting.name = 'NEW NAME';
+accounting.printEmployeeInformation();
 
-const accountingCopy = { name: 'DUMMEY', describe: accounting.describe };
-accountingCopy.describe();
+// const accountingCopy = { name: 'DUMMEY', describe: accounting.describe };
+// accountingCopy.describe();
