e2dd0144f1b07e82db30896b4250cf0d3e5a7ab8
Ophir Yael <ofir.yael@gmail.com>
2020-02-02 23:55:41 +0200

    Add readonly property

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ class Department {
   // private name: string;
   private employees: string[] = [];
 
-  constructor(private id: string, public name: string) {
+  constructor(private readonly id: string, public name: string) {
     // this.id = id;
     // this.name = n;
   }
@@ -13,6 +13,7 @@ class Department {
   }
 
   addEmployee(employee: string) {
+    // this.id = 'd2';
     this.employees.push(employee);
   }
 
