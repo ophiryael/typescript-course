1ca4afe0f570a54e19fea053f89cd38b19f42b69
Ophir Yael <ofir.yael@gmail.com>
2020-02-08 17:31:15 +0200

    Use optional parameters and properties

--- dist/app.js
+++ dist/app.js
@@ -6,15 +6,22 @@ add = function (n1, n2) {
 var Person = /** @class */ (function () {
     function Person(n) {
         this.age = 9001;
-        this.name = n;
+        if (n) {
+            this.name = n;
+        }
     }
     Person.prototype.greet = function (phrase) {
-        console.log(phrase + " " + this.name);
+        if (this.name) {
+            console.log(phrase + " " + this.name);
+        }
+        else {
+            console.log('Hi!');
+        }
     };
     return Person;
 }());
 var user1;
-user1 = new Person('Ophir');
+user1 = new Person();
 // user1.name = 'Chewbacca';
 user1.greet('Hi there - I am');
 console.log(user1);
--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,8 @@ add = (n1: number, n2: number) => {
 };
 
 interface Named {
-  readonly name: string;
+  readonly name?: string;
+  outputName?: string;
 }
 
 interface Greetable extends Named {
@@ -17,20 +18,26 @@ interface Greetable extends Named {
 }
 
 class Person implements Greetable {
-  name: string;
+  name?: string;
   age = 9001;
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(n?: string) {
+    if (n) {
+      this.name = n;
+    }
   }
 
   greet(phrase: string) {
-    console.log(`${phrase} ${this.name}`);
+    if (this.name) {
+      console.log(`${phrase} ${this.name}`);
+    } else {
+      console.log('Hi!');
+    }
   }
 }
 
 let user1: Greetable;
-user1 = new Person('Ophir');
+user1 = new Person();
 // user1.name = 'Chewbacca';
 user1.greet('Hi there - I am');
 console.log(user1);
