3534b06d31935cada32f58b10a59885d230c93b3
Ophir Yael <ofir.yael@gmail.com>
2020-02-15 19:00:49 +0200

    Create generic function

--- src/app.ts
+++ src/app.ts
@@ -10,3 +10,15 @@
 // promise.then(data => {
 //   data.split(' ');
 // });
+
+function merge<T, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+// const mergedObj = merge<{ name: string; hobbies: string[] }, { age: number }>(
+//   { name: 'Gandalf', hobbies: ['Sports'] },
+//   { age: 2019 }
+// );
+
+const mergedObj = merge({ name: 'Gandalf', hobbies: ['Sports'] }, { age: 2019 });
+console.log(mergedObj.age);
