5dc3fff02ff18876fd1bf66cb45703722bbb4ba3
Ophir Yael <ofir.yael@gmail.com>
2020-02-15 19:19:18 +0200

    Use generic constraints

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@
 //   data.split(' ');
 // });
 
-function merge<T, U>(objA: T, objB: U) {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 
@@ -21,4 +21,4 @@ function merge<T, U>(objA: T, objB: U) {
 // );
 
 const mergedObj = merge({ name: 'Gandalf', hobbies: ['Sports'] }, { age: 2019 });
-console.log(mergedObj.age);
+console.log(mergedObj);
