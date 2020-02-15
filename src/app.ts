09bcb266ee4120fb265a2e54240b5338c9f71ed9
Ophir Yael <ofir.yael@gmail.com>
2020-02-15 21:02:15 +0200

    Use generic utility types

--- src/app.ts
+++ src/app.ts
@@ -79,3 +79,20 @@ const numberStorage = new DataStorage<number>();
 // // ...
 // objStorage.removeItem(ronObj);
 // console.log(objStorage.getItems());
+
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUntil: Date;
+}
+
+function createCourseGoal(title: string, description: string, date: Date): CourseGoal {
+  let courseGoal: Partial<CourseGoal> = {};
+  courseGoal.title = title;
+  courseGoal.description = description;
+  courseGoal.completeUntil = date;
+  return courseGoal as CourseGoal;
+}
+
+const names: Readonly<string[]> = ['Ron', 'Harry'];
+// names.push('Neo');
