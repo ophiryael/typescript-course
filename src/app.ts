30281361c637e8cef5387983350c940b88f4ccfd
Ophir Yael <ofir.yael@gmail.com>
2020-04-25 20:41:19 +0300

    Create AutoBind decorator

--- src/app.ts
+++ src/app.ts
@@ -88,3 +88,31 @@ class Product {
     return this._price * (1 + tax);
   }
 }
+
+function AutoBind(_target: any, _methodName: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
+class Printer {
+  message = 'This works!';
+
+  @AutoBind
+  showMessage() {
+    console.log(this.message);
+  }
+}
+
+const p = new Printer();
+p.showMessage();
+
+const button = document.querySelector('button')!;
+button.addEventListener('click', p.showMessage);
