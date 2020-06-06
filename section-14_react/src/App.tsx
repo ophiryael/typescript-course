500d839415d428df685a0b6f107160c480b7b2da
Ophir Yael <ofir.yael@gmail.com>
2020-06-06 19:37:19 +0300

    Define types for props

--- section-14_react/src/App.tsx
+++ section-14_react/src/App.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import { TodoList } from './components/TodoList';
 
 export const App: React.FC = () => {
-  return <div className="App"></div>;
+  const todos = [{ id: 't1', text: 'Learn TypeScript' }];
+
+  return (
+    <div className="App">
+      <TodoList items={todos} />
+    </div>
+  );
 };
---
+++ section-14_react/src/components/TodoList.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface TodoListProps {
+  items: { id: string; text: string }[];
+}
+
+export const TodoList: React.FC<TodoListProps> = (props) => {
+  return (
+    <ul>
+      {props.items.map((todo) => (
+        <li key={todo.id}>{todo.text}</li>
+      ))}
+    </ul>
+  );
+};
