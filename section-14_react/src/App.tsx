f845da5da63293dcfdb9961f14189bced8836063
Ophir Yael <ofir.yael@gmail.com>
2020-06-12 11:34:12 +0300

    Update todos with useState

--- section-14_react/src/App.tsx
+++ section-14_react/src/App.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NewTodo } from './components/NewTodo';
 import { TodoList } from './components/TodoList';
+import { Todo } from './todo.model';
 
 export const App: React.FC = () => {
-  const todos = [{ id: 't1', text: 'Learn TypeScript' }];
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   const todoAddHandler = (text: string) => {
-    console.log(text);
+    const newTodo = { id: Date.now().toString(), text };
+    setTodos((prevTodos) => [...prevTodos, newTodo]);
   };
 
   return (
